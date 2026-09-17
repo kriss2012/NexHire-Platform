f4957fc234ab0c056890efd8de4896668e72bb78
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-09-17 09:06:58 +0530

    fix(backend): separate production tsconfig from test tsconfig with jest types

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
   collectCoverageFrom: [
     'src/**/*.ts',
